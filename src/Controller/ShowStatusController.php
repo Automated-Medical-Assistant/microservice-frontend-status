8c0f32a079f50d5207075a834da7e2ecb1965dcb
Steven Zemelka <steven.zemelka@gmail.com>
2020-03-22 16:22:25 +0100

    current state

---
+++ src/Controller/ShowStatusController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpClient\HttpClient;
+
+class ShowStatusController extends AbstractController
+{
+    /**
+     * @Route("/show/status", name="show_status")
+     */
+    public function index()
+    {
+
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'http://127.0.0.1:8008/status/15848432139');
+
+        // the template path is the relative file path from `templates/`
+        return $this->render('status/show.status.html.twig', [
+            // this array defines the variables passed to the template,
+            // where the key is the variable name and the value is the variable value
+            // (Twig recommends using snake_case variable names: 'foo_bar' instead of 'fooBar')
+            'state' => $response->toArray()['message'],
+        ]);
+    }
+}
